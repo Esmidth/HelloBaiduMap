44aa429e7017b7b5564f576767d14426dbd8db40
Qingyi Chang <esmidth@163.com>
2015-12-27 22:53:43 +0800

    implement MylocationListener & initLocation method.

--- app/src/main/java/com/example/esmidth/hellobaidumap/MainActivity.java
+++ app/src/main/java/com/example/esmidth/hellobaidumap/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.esmidth.hellobaidumap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.MapView;
 
+
 public class MainActivity extends AppCompatActivity {
     MapView mapView = null;
+    public LocationClient locationClient = null;
+    public BDLocationListener mylistener = new MyLocationListener();
 
     /* TODO: Add Location Function to the App*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +24,33 @@ public class MainActivity extends AppCompatActivity {
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         mapView = (MapView) findViewById(R.id.bmapView);
+        locationClient = new LocationClient(getApplicationContext());
+        locationClient.registerLocationListener(mylistener);
+        initLocation();
+
+        locationClient.start();
 
     }
+
+
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认false，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        locationClient.setLocOption(option);
+    }
+
 
     @Override
     protected void onResume() {
